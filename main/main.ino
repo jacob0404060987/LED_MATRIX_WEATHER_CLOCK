2697a4661583615ee700f914df737c67c2686ace
Jacob0404060987 <kubapaw1256@gmail.com>
2023-04-28 00:39:47 +0200

    Json rozpakowany bez filtracji

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,14 @@
 //libraries
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <Arduino_JSON.h>
+#include <ArduinoJson.h>
 #include "time.h"
 //constants
 //WiFI setup
 const char* ssid = "TAJNA JASKINA BABCI JADZI";
 const char* passwd = "Morys123";
+//const char* ssid = "hamilton krul";
+//const char* passwd = "qwerty1234";
 //time setup
 const char* ntpServer = "europe.pool.ntp.org";
 const long gmt_strefa = 3600; //GMT+1
@@ -14,6 +16,7 @@ const int daylightsave = 3600;
 //weather setup
 const String weather_link = "http://api.openweathermap.org/data/2.5/weather?q=Krakow,pl&APPID=";
 const String api_key = "0e11ccad42e110d8beb9583b9da92a4d";
+float temperatura;
 void printTime()
 {
   struct tm timeinfo;
@@ -27,30 +30,15 @@ void printTime()
   }
 }
 
-bool readReponseContent(struct clientData* clientData) {
-  // Compute optimal size of the JSON buffer according to what we need to parse.
-  // See https://bblanchon.github.io/ArduinoJson/assistant/
-  const size_t bufferSize = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(1) + 
-      2*JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + 
-      JSON_OBJECT_SIZE(6) + JSON_OBJECT_SIZE(12) + 390;
-  DynamicJsonBuffer jsonBuffer(bufferSize);
 
-  JsonObject& root = jsonBuffer.parseObject(client);
-}
-  if (!root.success()) {
-    Serial.println("JSON parsing failed!");
-    return false;
-  }
 
   // Here were copy the strings we're interested in using to your struct data
-      strcpy(clientData->temp, root["main"]["temp"]);
-  strcpy(clientData->humidity, root["main"]["humidity"]);
   // It's not mandatory to make a copy, you could just use the pointers
   // Since, they are pointing inside the "content" buffer, so you need to make
   // sure it's still in memory when you read the string
 
-  return true;
-}
+  
+
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid,passwd);
@@ -68,134 +56,9 @@ void setup() {
   http.begin(weather_link + api_key);
   int httpCode = http.GET();
   String payload=http.getString();
-}
+  StaticJsonDocument<1024> doc;
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  
-  //printTime();
-
-  Serial.println(payload);
- JSONVar my_json = JSON.parse(payload);
- JSONVar keys = my_json.keys();
- Serial.println("Dana z Dzejsona");
-   Serial.printf("\n");
-
- delay(30000);
-}
-
-
-/*Json from weather API
-{
-  "coord": {
-    "lon": 19.9167,
-    "lat": 50.0833
-  },
-  "weather": [
-    {
-      "id": 802,
-      "main": "Clouds",
-      "description": "scattered clouds",
-      "icon": "03n"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 285.99,
-    "feels_like": 285.21,
-    "temp_min": 285.26,
-    "temp_max": 286.78,
-    "pressure": 1004,
-    "humidity": 72
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 3.6,
-    "deg": 250
-  },
-  "clouds": {
-    "all": 40
-  },
-  "dt": 1682361409,
-  "sys": {
-    "type": 2,
-    "id": 2009211,
-    "country": "PL",
-    "sunrise": 1682307007,
-    "sunset": 1682358398
-  },
-  "timezone": 7200,
-  "id": 3094802,
-  "name": "Krakow",
-  "cod": 200
-}
-JSON filter:
-{
-  "coord": false,
-  "weather": [
-    {
-      "id": false,
-      "main": true,
-      "description": false,
-      "icon": false
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": true,
-    "feels_like": false,
-    "temp_min": false,
-    "temp_max": false,
-    "pressure": true,
-    "humidity": true
-  },
-  "visibility": false,
-  "wind": false,
-  "clouds": false,
-  "dt": false,
-  "sys": false,
-  "timezone": false,
-  "id": false,
-  "name": false,
-  "cod": false
-}
-*/
-
-
-//filterd JSON parse code
-/*
-// String payload;
-
-StaticJsonDocument<112> filter;
-filter["coord"] = false;
-
-JsonObject filter_weather_0 = filter["weather"].createNestedObject();
-filter_weather_0["id"] = false;
-filter_weather_0["main"] = true;
-filter_weather_0["description"] = false;
-filter_weather_0["icon"] = false;
-filter["base"] = "stations";
-
-JsonObject filter_main = filter.createNestedObject("main");
-filter_main["temp"] = true;
-filter_main["feels_like"] = false;
-filter_main["temp_min"] = false;
-filter_main["temp_max"] = false;
-filter_main["pressure"] = true;
-filter_main["humidity"] = true;
-filter["visibility"] = false;
-filter["wind"] = false;
-filter["clouds"] = false;
-filter["dt"] = false;
-filter["sys"] = false;
-filter["timezone"] = false;
-filter["id"] = false;
-filter["name"] = false;
-filter["cod"] = false;
-
-StaticJsonDocument<192> doc;
-
-DeserializationError error = deserializeJson(doc, payload, DeserializationOption::Filter(filter));
+DeserializationError error = deserializeJson(doc, payload);
 
 if (error) {
   Serial.print("deserializeJson() failed: ");
@@ -203,11 +66,56 @@ if (error) {
   return;
 }
 
-const char* weather_0_main = doc["weather"][0]["main"]; // "Clouds"
+float coord_lon = doc["coord"]["lon"]; // 19.9167
+float coord_lat = doc["coord"]["lat"]; // 50.0833
+
+JsonObject weather_0 = doc["weather"][0];
+int weather_0_id = weather_0["id"]; // 802
+const char* weather_0_main = weather_0["main"]; // "Clouds"
+const char* weather_0_description = weather_0["description"]; // "scattered clouds"
+const char* weather_0_icon = weather_0["icon"]; // "03n"
+
+const char* base = doc["base"]; // "stations"
 
 JsonObject main = doc["main"];
-float main_temp = main["temp"]; // 285.99
+temperatura = main["temp"]; // 285.99
+float main_feels_like = main["feels_like"]; // 285.21
+float main_temp_min = main["temp_min"]; // 285.26
+float main_temp_max = main["temp_max"]; // 286.78
 int main_pressure = main["pressure"]; // 1004
 int main_humidity = main["humidity"]; // 72
 
-*/
+int visibility = doc["visibility"]; // 10000
+
+float wind_speed = doc["wind"]["speed"]; // 3.6
+int wind_deg = doc["wind"]["deg"]; // 250
+
+int clouds_all = doc["clouds"]["all"]; // 40
+
+long dt = doc["dt"]; // 1682361409
+
+JsonObject sys = doc["sys"];
+int sys_type = sys["type"]; // 2
+long sys_id = sys["id"]; // 2009211
+const char* sys_country = sys["country"]; // "PL"
+long sys_sunrise = sys["sunrise"]; // 1682307007
+long sys_sunset = sys["sunset"]; // 1682358398
+
+int timezone = doc["timezone"]; // 7200
+long id = doc["id"]; // 3094802
+const char* name = doc["name"]; // "Krakow"
+int cod = doc["cod"]; // 200
+
+
+  
+}
+// String input;
+void loop()
+{
+  Serial.print("temperatura to");
+  Serial.println(temperatura);
+  delay(30000);
+}
+
+
+
